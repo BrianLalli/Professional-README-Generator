2d0f8ba618b8344890830e833615c319c04020b3
Brian Lalli <106120015+BrianLalli@users.noreply.github.com>
2022-07-18 16:58:25 -0500

    created packages, array for questions, function to write README, function to initialize app, and function to call app, generated markdown, added table of contents and license icon

---
+++ Develop/generatedREADME.md
@@ -0,0 +1,34 @@
+# snqk
+  
+
+  ## Description
+  xnsk
+
+  ## Table of Contents
+  <!--ts-->
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  <!--te-->
+
+  ## Installation
+  cns
+
+  ## Usage
+  cnbs
+
+  ## License
+  None
+
+  ## Contributing
+  cbs
+
+  ## Tests
+  nbcks
+
+  ## Questions
+  GitHub:nbcsk
+  Email:bks
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,79 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateREADME = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // inquirer.prompt ([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What packages need to be installed to run your program?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Please input your directions for usage',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose your license from the options below:',
+        choices: ['None', 'MIT', 'Mozilla Public License 2.0'],
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'List all collaborators',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Write out any tests for this program',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your github username.',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address.',
+      },
+    ]
+
+    // generateMarkdown(data);
+
+    function init() {
+        inquirer.prompt(questions)
+        .then((answers) => {
+            console.log(answers);
+            // make readme string
+            let createREADME = generateREADME(answers);
+            console.log(createREADME);
+            fs.writeFile('generatedREADME.md', createREADME, function (err) {
+            console.log('success message');
+            })
+        });
+    };
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+
 
 // Function call to initialize app
 init();
